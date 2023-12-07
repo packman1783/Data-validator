54fd28055127ab7d52b5df6f2fe774b559c77132
Saplitsyn Denis <packman17@mail.ru>
2023-12-07 13:45:28 +0300

    create string Validator add empty StringSchema

---
+++ app/src/main/java/hexlet/code/Validator.java
@@ -0,0 +1,9 @@
+package hexlet.code;
+
+import hexlet.code.shemas.StringSchema;
+
+public class Validator {
+    public StringSchema string() {
+        return new StringSchema();
+    }
+}
---
+++ app/src/main/java/hexlet/code/shemas/StringSchema.java
@@ -0,0 +1,5 @@
+package hexlet.code.shemas;
+
+public class StringSchema {
+
+}
