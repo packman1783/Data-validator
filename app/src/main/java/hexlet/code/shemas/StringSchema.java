4dc2ab84f59427f8a8c0375de7c6bb36adf1a791
Saplitsyn Denis <packman17@mail.ru>
2023-12-07 16:03:13 +0300

    add StringSchema

--- app/src/main/java/hexlet/code/shemas/StringSchema.java
+++ app/src/main/java/hexlet/code/shemas/StringSchema.java
@@ -1,5 +1,35 @@
 package hexlet.code.shemas;
 
 public class StringSchema {
+    private boolean required;
+    private int minLength;
+    private String containsSubstring;
 
+    public StringSchema required() {
+        this.required = true;
+        return this;
+    }
+
+    public StringSchema minLength(int length) {
+        this.minLength = length;
+        return this;
+    }
+
+    public StringSchema contains(String substring) {
+        this.containsSubstring = substring;
+        return this;
+    }
+
+    public boolean isValid(String text) {
+        if (required && (text == null || text.isEmpty())) {
+            return false;
+        }
+        if (text != null && text.length() < minLength) {
+            return false;
+        }
+        if (containsSubstring != null && text != null && !text.contains(containsSubstring)) {
+            return false;
+        }
+        return true;
+    }
 }
