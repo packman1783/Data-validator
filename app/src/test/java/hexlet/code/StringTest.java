480d74581a32e922179fe41461319bdfb696871f
Saplitsyn Denis <packman17@mail.ru>
2023-12-07 17:40:33 +0300

    add StringTest

---
+++ app/src/test/java/hexlet/code/StringTest.java
@@ -0,0 +1,42 @@
+package hexlet.code;
+
+import hexlet.code.shemas.StringSchema;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class StringTest {
+    private StringSchema schema;
+    private Validator validator;
+
+    @BeforeEach
+    public void beforeEach() {
+        validator = new Validator();
+        schema = validator.string();
+    }
+
+    @Test
+    public void validTest() {
+        assertTrue(schema.isValid(null));
+        assertTrue(schema.isValid(""));
+        schema.required();
+        assertFalse(schema.isValid(null));
+        assertFalse(schema.isValid(""));
+    }
+
+    @Test
+    public void minLengthTest() {
+        schema.minLength(5);
+        assertTrue(schema.isValid("hello"));
+        assertFalse(schema.isValid("what"));
+    }
+
+    @Test
+    public void containTest() {
+        schema.contains("wh");
+        assertTrue(schema.isValid("what"));
+        assertFalse(schema.isValid("wat"));
+    }
+}
