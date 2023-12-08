cb87ccaa1c268fb30188a2e47d20f2edf028f9ac
Saplitsyn Denis <packman17@mail.ru>
2023-12-08 15:38:55 +0300

    change NumberSchema

--- app/src/main/java/hexlet/code/shemas/NumberSchema.java
+++ app/src/main/java/hexlet/code/shemas/NumberSchema.java
@@ -1,5 +1,38 @@
 package hexlet.code.shemas;
 
 public class NumberSchema {
+    private boolean required;
+    private boolean positive;
+    private int minNumber;
+    private int maxNumber;
 
+    public NumberSchema required() {
+        this.required = true;
+        return this;
+    }
+
+    public NumberSchema positive() {
+        this.positive = true;
+        return this;
+    }
+
+    public NumberSchema range(int minNum, int maxNum) {
+        this.minNumber = minNum;
+        this.maxNumber = maxNum;
+        return this;
+    }
+
+    public boolean isValid(int number) {
+        if (required && number == 0) {
+            return false;
+        }
+        if (positive && number <= 0 ) {
+            return false;
+        }
+        if (number < minNumber || number > maxNumber) {
+            return false;
+        }
+
+        return true;
+    }
 }
