5aaf4443d8f3c321f070ece916facf45c41a75a3
Saplitsyn Denis <packman17@mail.ru>
2023-12-08 14:38:04 +0300

    add NumberSchema in validator and create empty NumberSchema

--- app/src/main/java/hexlet/code/Validator.java
+++ app/src/main/java/hexlet/code/Validator.java
@@ -1,9 +1,14 @@
 package hexlet.code;
 
+import hexlet.code.shemas.NumberSchema;
 import hexlet.code.shemas.StringSchema;
 
 public class Validator {
     public StringSchema string() {
         return new StringSchema();
+    }
+
+    public NumberSchema number() {
+        return new NumberSchema();
     }
 }
