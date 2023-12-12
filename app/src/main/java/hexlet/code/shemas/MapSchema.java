344e3d1f300ccc5f7fd84bdf41e61ed31d6e2d1a
Saplitsyn Denis <packman17@mail.ru>
2023-12-12 14:27:57 +0300

    change for checkstyle need whitespace

--- app/src/main/java/hexlet/code/shemas/MapSchema.java
+++ app/src/main/java/hexlet/code/shemas/MapSchema.java
@@ -15,10 +15,10 @@ public class MapSchema extends BaseSchema {
         if (data == null) {
             return !required;
         }
-        if (required && ((Map)data).isEmpty()) {
+        if (required && ((Map) data).isEmpty()) {
             return false;
         }
-        if (expectedSize != null && ((Map)data).size() != expectedSize) {
+        if (expectedSize != null && ((Map) data).size() != expectedSize) {
             return false;
         }
 
