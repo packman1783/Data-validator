4bd275c92e0fadd3a01b616831b75d9e440963e0
Saplitsyn Denis <packman17@mail.ru>
2023-12-12 14:21:10 +0300

    add MapSchema in validator

--- app/src/main/java/hexlet/code/Validator.java
+++ app/src/main/java/hexlet/code/Validator.java
@@ -1,5 +1,6 @@
 package hexlet.code;
 
+import hexlet.code.shemas.MapSchema;
 import hexlet.code.shemas.NumberSchema;
 import hexlet.code.shemas.StringSchema;
 
@@ -10,5 +11,9 @@ public class Validator {
 
     public NumberSchema number() {
         return new NumberSchema();
+    }
+
+    public MapSchema map() {
+        return new MapSchema();
     }
 }
