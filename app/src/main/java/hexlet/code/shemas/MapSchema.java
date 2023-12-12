e43aa36dbfee0d3c1f48aab631b6e0519cbb07ed
Saplitsyn Denis <packman17@mail.ru>
2023-12-12 14:19:33 +0300

    add MapSchema

---
+++ app/src/main/java/hexlet/code/shemas/MapSchema.java
@@ -0,0 +1,27 @@
+package hexlet.code.shemas;
+
+import java.util.Map;
+
+public class MapSchema extends BaseSchema {
+    private Integer expectedSize = null;
+
+    public MapSchema sizeOf(int size) {
+        this.expectedSize = size;
+        return this;
+    }
+
+    @Override
+    public boolean isValid(Object data) {
+        if (data == null) {
+            return !required;
+        }
+        if (required && ((Map)data).isEmpty()) {
+            return false;
+        }
+        if (expectedSize != null && ((Map)data).size() != expectedSize) {
+            return false;
+        }
+
+        return true;
+    }
+}
