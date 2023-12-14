6d6ee08699e8caa078722ddaa4699ac28521b236
Saplitsyn Denis <packman17@mail.ru>
2023-12-14 17:30:51 +0300

    refactor StringSchema: check in parent constructor instance of schema, replace checking from inValid() to same name methods schema add method required

--- app/src/main/java/hexlet/code/shemas/StringSchema.java
+++ app/src/main/java/hexlet/code/shemas/StringSchema.java
@@ -1,31 +1,43 @@
 package hexlet.code.shemas;
 
 public class StringSchema extends BaseSchema {
-    private int minLength;
-    private String containsSubstring;
+    public StringSchema() {
+        super();
+        Object statement = input -> {
+            if (input == null || input instanceof String) {
+                return true;
+            }
+        };
+        addRules("string", statement);
+    }
+
+    public StringSchema required() {
+        Object statement = input -> {
+            if(input != null && !input.equals("")) {
+                return true;
+            }
+        };
+        addRules("required", statement);
+        return this;
+    }
 
     public StringSchema minLength(int length) {
-        this.minLength = length;
+        Object statement = input -> {
+            if(input != null && ((String) input).length() >= length) {
+                return true;
+            }
+        };
+        addRules("minLength", statement);
         return this;
     }
 
     public StringSchema contains(String substring) {
-        this.containsSubstring = substring;
+        Object statement = input -> {
+            if (input != null && ((String) input).contains(substring)) {
+                return true;
+            }
+        };
+        addRules("contains", statement);
         return this;
-    }
-
-    @Override
-    public boolean isValid(Object data) {
-        String text = (String) data;
-        if (required && (text == null || text.isEmpty())) {
-            return false;
-        }
-        if (text != null && text.length() < minLength) {
-            return false;
-        }
-        if (containsSubstring != null && text != null && !text.contains(containsSubstring)) {
-            return false;
-        }
-        return true;
     }
 }
