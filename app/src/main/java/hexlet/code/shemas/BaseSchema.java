4f0e6d813372adb9c50b398a1ee5948a6e1fbef4
Saplitsyn Denis <packman17@mail.ru>
2023-12-18 16:49:58 +0300

    change boolean type on validationRule interface, add isValid method which use checkRule method with function interface method apply

--- app/src/main/java/hexlet/code/shemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/shemas/BaseSchema.java
@@ -3,16 +3,29 @@ package hexlet.code.shemas;
 import java.util.HashMap;
 import java.util.Map;
 
-public abstract class BaseSchema {
-    private final Map<String, Object> rules;
+public class BaseSchema {
+    private final Map<String, ValidationRule> rules;
 
     public BaseSchema() {
         rules = new HashMap<>();
     }
 
-    public void addRules(String name, Object statement) {
-        rules.put(name, statement);
+    public void addRules(String name, ValidationRule rule) {
+        rules.put(name, rule);
     }
 
-    public boolean isValid(Object data);
+    public boolean isValid(Object data) {
+        for (Map.Entry<String, ValidationRule> entry : rules.entrySet()) {
+            String nameRules = entry.getKey();
+            ValidationRule rule = entry.getValue();
+            if (!checkRule(rule, data)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean checkRule(ValidationRule rule, Object data) {
+        return rule.apply(data);
+    }
 }
