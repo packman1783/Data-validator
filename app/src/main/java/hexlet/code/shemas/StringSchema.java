d055f7401c8efd8d19c72a0c2532de7f170ba923
Saplitsyn Denis <packman17@mail.ru>
2023-12-20 15:29:21 +0300

    change for checkstyle

--- app/src/main/java/hexlet/code/shemas/StringSchema.java
+++ app/src/main/java/hexlet/code/shemas/StringSchema.java
@@ -3,45 +3,28 @@ package hexlet.code.shemas;
 public class StringSchema extends BaseSchema {
     public StringSchema() {
         super();
-        ValidationRule stringRule = input -> {
-            if (input == null || input instanceof String) {
-                return true;
-            }
-            return false;
-        };
+        ValidationRule stringRule = input -> input == null || input instanceof String;
         addRules("string", stringRule);
     }
 
     public StringSchema required() {
-        ValidationRule requiredRule = input -> {
-            if (input != null && !((String) input).isEmpty()) {
-                return true;
-            }
-            return false;
-        };
+        ValidationRule requiredRule = input -> input != null && !((String) input).isEmpty();
         addRules("required", requiredRule);
+
         return this;
     }
 
     public StringSchema minLength(int length) {
-        ValidationRule minLangthRule = input -> {
-            if (input != null && ((String) input).length() >= length) {
-                return true;
-            }
-            return false;
-        };
+        ValidationRule minLangthRule = input -> input != null && ((String) input).length() >= length;
         addRules("minLength", minLangthRule);
+
         return this;
     }
 
     public StringSchema contains(String substring) {
-        ValidationRule containsRule = input -> {
-            if (input != null && ((String) input).contains(substring)) {
-                return true;
-            }
-            return false;
-        };
+        ValidationRule containsRule = input -> input != null && ((String) input).contains(substring);
         addRules("contains", containsRule);
+
         return this;
     }
 }
