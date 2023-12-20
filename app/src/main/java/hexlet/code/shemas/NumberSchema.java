2866a5d870ff4e55bdb52d2eba3eb7251bc38289
Saplitsyn Denis <packman17@mail.ru>
2023-12-20 15:29:50 +0300

    change for checkstyle

--- app/src/main/java/hexlet/code/shemas/NumberSchema.java
+++ app/src/main/java/hexlet/code/shemas/NumberSchema.java
@@ -3,40 +3,29 @@ package hexlet.code.shemas;
 public class NumberSchema extends BaseSchema {
     public NumberSchema() {
         super();
-        ValidationRule integerRule = input -> {
-            if (input == null || input instanceof Integer) {
-                return true;
-            }
-            return false;
-        };
+        ValidationRule integerRule = input -> input == null || input instanceof Integer;
         addRules("integer", integerRule);
     }
 
     public NumberSchema required() {
         ValidationRule requiredRule = input -> input != null;
         addRules("required", requiredRule);
+
         return this;
     }
 
     public NumberSchema positive() {
-        ValidationRule positiveRule = input -> {
-            if (input instanceof Integer && (Integer) input > 0) {
-                return true;
-            }
-            return false;
-        };
+        ValidationRule positiveRule = input -> input instanceof Integer && (Integer) input > 0;
         addRules("positive", positiveRule);
+
         return this;
     }
 
     public NumberSchema range(int minNum, int maxNum) {
-        ValidationRule rangeRule = input -> {
-            if (input instanceof Integer && (Integer) input >= minNum && (Integer) input <= maxNum) {
-                return true;
-            }
-            return false;
-        };
+        ValidationRule rangeRule = input -> input instanceof Integer
+                && (Integer) input >= minNum && (Integer) input <= maxNum;
         addRules("range", rangeRule);
+
         return this;
     }
 }
