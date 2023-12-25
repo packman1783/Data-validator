e9db4089f6c8977d5cf5981cdd6cbeb31cc67be0
Saplitsyn Denis <packman17@mail.ru>
2023-12-25 13:42:02 +0300

    change type (interface) validationRule on Predicate<Object>

--- app/src/main/java/hexlet/code/schemas/StringSchema.java
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -1,28 +1,30 @@
 package hexlet.code.schemas;
 
+import java.util.function.Predicate;
+
 public class StringSchema extends BaseSchema {
     public StringSchema() {
         super();
-        ValidationRule stringRule = input -> input == null || input instanceof String;
+        Predicate<Object> stringRule = input -> input == null || input instanceof String;
         addRules("string", stringRule);
     }
 
     public StringSchema required() {
-        ValidationRule requiredRule = input -> input != null && !((String) input).isEmpty();
+        Predicate<Object> requiredRule = input -> input != null && !((String) input).isEmpty();
         addRules("required", requiredRule);
 
         return this;
     }
 
     public StringSchema minLength(int length) {
-        ValidationRule minLangthRule = input -> input != null && ((String) input).length() >= length;
+        Predicate<Object> minLangthRule = input -> input != null && ((String) input).length() >= length;
         addRules("minLength", minLangthRule);
 
         return this;
     }
 
     public StringSchema contains(String substring) {
-        ValidationRule containsRule = input -> input != null && ((String) input).contains(substring);
+        Predicate<Object> containsRule = input -> input != null && ((String) input).contains(substring);
         addRules("contains", containsRule);
 
         return this;
