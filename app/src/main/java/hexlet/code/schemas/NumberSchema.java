9540f751519b90caea785824d6b8c2a1708626d5
Saplitsyn Denis <packman17@mail.ru>
2023-12-25 13:42:28 +0300

    change type (interface) validationRule on Predicate<Object>

--- app/src/main/java/hexlet/code/schemas/NumberSchema.java
+++ app/src/main/java/hexlet/code/schemas/NumberSchema.java
@@ -1,30 +1,31 @@
 package hexlet.code.schemas;
 
 import java.util.Objects;
+import java.util.function.Predicate;
 
 public class NumberSchema extends BaseSchema {
     public NumberSchema() {
         super();
-        ValidationRule integerRule = input -> input == null || input instanceof Integer;
+        Predicate<Object> integerRule = input -> input == null || input instanceof Integer;
         addRules("integer", integerRule);
     }
 
     public NumberSchema required() {
-        ValidationRule requiredRule = Objects::nonNull;
+        Predicate<Object> requiredRule = Objects::nonNull;
         addRules("required", requiredRule);
 
         return this;
     }
 
     public NumberSchema positive() {
-        ValidationRule positiveRule = input -> input instanceof Integer && (Integer) input > 0;
+        Predicate<Object> positiveRule = input -> input instanceof Integer && (Integer) input > 0;
         addRules("positive", positiveRule);
 
         return this;
     }
 
     public NumberSchema range(int minNum, int maxNum) {
-        ValidationRule rangeRule = input -> input instanceof Integer
+        Predicate<Object> rangeRule = input -> input instanceof Integer
                 && (Integer) input >= minNum && (Integer) input <= maxNum;
         addRules("range", rangeRule);
 
