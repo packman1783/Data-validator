7d10209ff35d09fc0391262edf5e765450a89779
Saplitsyn Denis <packman17@mail.ru>
2023-12-25 13:45:12 +0300

    change type (interface) validationRule on Predicate<Object>, instead method checkRule use inner Predicate method test

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -2,29 +2,26 @@ package hexlet.code.schemas;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Predicate;
 
 public class BaseSchema {
-    private final Map<String, ValidationRule> rules;
+    private final Map<String, Predicate<Object>> rules;
 
     public BaseSchema() {
         rules = new HashMap<>();
     }
 
-    public void addRules(String name, ValidationRule rule) {
+    public void addRules(String name, Predicate<Object> rule) {
         rules.put(name, rule);
     }
 
     public boolean isValid(Object data) {
-        for (Map.Entry<String, ValidationRule> entry : rules.entrySet()) {
-            ValidationRule rule = entry.getValue();
-            if (!checkRule(rule, data)) {
+        for (Map.Entry<String, Predicate<Object>> entry : rules.entrySet()) {
+            Predicate<Object> rule = entry.getValue();
+            if (!rule.test(data)) {
                 return false;
             }
         }
         return true;
-    }
-
-    private boolean checkRule(ValidationRule rule, Object data) {
-        return rule.apply(data);
     }
 }
