78ca7694555060b468f40d9e6d72bee02a19aa40
Saplitsyn Denis <packman17@mail.ru>
2024-02-05 19:57:04 +0300

    refactor MapSchema, use generic <T>

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -4,31 +4,31 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.function.Predicate;
 
-public final class MapSchema extends BaseSchema {
+public final class MapSchema<T extends BaseSchema> extends BaseSchema {
     public MapSchema() {
         Predicate<Object> mapRule = input -> input == null || input instanceof Map;
         addRules("map", mapRule);
     }
 
-    public MapSchema required() {
+    public MapSchema<T> required() {
         Predicate<Object> requiredRule = Objects::nonNull;
         addRules("required", requiredRule);
 
         return this;
     }
 
-    public MapSchema sizeof(int size) {
+    public MapSchema<T> sizeof(int size) {
         Predicate<Object> sizeOfRule = input -> ((Map<?, ?>) input).size() == size;
         addRules("sizeOf", sizeOfRule);
 
         return this;
     }
 
-    public MapSchema shape(Map<String, BaseSchema> shape) {
+    public MapSchema<T> shape(Map<String, T> shape) {
         Predicate<Object> shapeRule = input -> shape.entrySet().stream()
                 .allMatch(entry -> {
                     String key = entry.getKey();
-                    BaseSchema value = entry.getValue();
+                    T value = entry.getValue();
                     return ((Map<?, ?>) input).containsKey(key) && value.isValid(((Map<?, ?>) input).get(key));
                 });
         addRules("shape", shapeRule);
