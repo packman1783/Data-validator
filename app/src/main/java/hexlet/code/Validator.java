527790c57aa93ad85b1fc2ab8243265120e4d5c4
Saplitsyn Denis <packman17@mail.ru>
2024-02-06 10:31:26 +0300

    rename <T> -> <V>

--- app/src/main/java/hexlet/code/Validator.java
+++ app/src/main/java/hexlet/code/Validator.java
@@ -14,7 +14,7 @@ public class Validator {
         return new NumberSchema();
     }
 
-    public final <T extends BaseSchema> MapSchema<T> map() {
+    public final <V extends BaseSchema> MapSchema<V> map() {
         return new MapSchema<>();
     }
 }
