d6dd3bff052d4c5b382d1e1586e9c5ab669dd5cb
Saplitsyn Denis <packman17@mail.ru>
2024-02-06 10:31:03 +0300

    rename <T> -> <V>

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -4,31 +4,31 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.function.Predicate;
 
-public final class MapSchema<T extends BaseSchema> extends BaseSchema {
+public final class MapSchema<V extends BaseSchema> extends BaseSchema {
     public MapSchema() {
         Predicate<Object> mapRule = input -> input == null || input instanceof Map;
         addRules("map", mapRule);
     }
 
-    public MapSchema<T> required() {
+    public MapSchema<V> required() {
         Predicate<Object> requiredRule = Objects::nonNull;
         addRules("required", requiredRule);
 
         return this;
     }
 
-    public MapSchema<T> sizeof(int size) {
+    public MapSchema<V> sizeof(int size) {
         Predicate<Object> sizeOfRule = input -> ((Map<?, ?>) input).size() == size;
         addRules("sizeOf", sizeOfRule);
 
         return this;
     }
 
-    public MapSchema<T> shape(Map<String, T> shape) {
+    public MapSchema<V> shape(Map<String, V> shape) {
         Predicate<Object> shapeRule = input -> shape.entrySet().stream()
                 .allMatch(entry -> {
                     String key = entry.getKey();
-                    T value = entry.getValue();
+                    V value = entry.getValue();
                     return ((Map<?, ?>) input).containsKey(key) && value.isValid(((Map<?, ?>) input).get(key));
                 });
         addRules("shape", shapeRule);
