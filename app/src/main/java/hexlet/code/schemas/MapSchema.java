6fd34e2add1b70838329237211b1ca51dd9a0c48
Saplitsyn Denis <packman17@mail.ru>
2024-02-13 02:14:16 +0300

    refactor method without Predicate<Object>

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -2,37 +2,29 @@ package hexlet.code.schemas;
 
 import java.util.Map;
 import java.util.Objects;
-import java.util.function.Predicate;
 
 public final class MapSchema extends BaseSchema<Map<?, ?>> {
     public MapSchema() {
-        Predicate<Object> mapRule = input -> input == null || input instanceof Map;
-        addRules("map", mapRule);
+        addRules("map", input -> input == null || input instanceof Map);
     }
 
     public MapSchema required() {
-        Predicate<Object> requiredRule = Objects::nonNull;
-        addRules("required", requiredRule);
-
+        addRules("required", Objects::nonNull);
         return this;
     }
 
     public MapSchema sizeof(int size) {
-        Predicate<Object> sizeOfRule = input -> ((Map<?, ?>) input).size() == size;
-        addRules("sizeOf", sizeOfRule);
-
+        addRules("sizeOf", input -> input instanceof Map && ((Map<?, ?>) input).size() == size);
         return this;
     }
 
     public <T> MapSchema shape(Map<String, BaseSchema<T>> shape) {
-        Predicate<Object> shapeRule = input -> shape.entrySet().stream()
+        addRules("shape", input -> input instanceof Map && shape.entrySet().stream()
                 .allMatch(entry -> {
                     String key = entry.getKey();
                     BaseSchema<T> value = entry.getValue();
                     return ((Map<?, ?>) input).containsKey(key) && value.isValid(((Map<?, ?>) input).get(key));
-                });
-        addRules("shape", shapeRule);
-
+                }));
         return this;
     }
 }
